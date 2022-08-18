7c584c9add66e4bb6bc0c57c29ea818db0b711a5
angel <angelt.corral@gmail.com>
2022-08-18 20:05:57 -0300

    carga de archivos iniciales

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+
+
+import {Login} from './Login';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+        <Login/>
+       
+    </React.StrictMode>
+)
